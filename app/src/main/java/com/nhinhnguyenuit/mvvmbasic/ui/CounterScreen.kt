eb641474018403f45a22e58309f45f037013261d
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2024-05-23 22:39:17 -0500

    Relace StateFlow by LiveData

--- app/src/main/java/com/nhinhnguyenuit/mvvmbasic/ui/CounterScreen.kt
+++ app/src/main/java/com/nhinhnguyenuit/mvvmbasic/ui/CounterScreen.kt
@@ -1,5 +1,6 @@
 package com.nhinhnguyenuit.mvvmbasic.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
@@ -7,26 +8,38 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import com.nhinhnguyenuit.mvvmbasic.viewmodel.CounterViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.nhinhnguyenuit.mvvmbasic.model.CounterState
 
 @Composable
 fun CounterScreen(viewModel: CounterViewModel = viewModel()) {
-    val state = viewModel.state.collectAsState()
-    val count = state.value.count
-    val name = state.value.name
-    val age = state.value.age.toString()
+    val state by viewModel.state.observeAsState(CounterState())
+    val count = state.count
+    val name = state.name
+    val age = state.age.toString()
 
-    Column {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(text = "Count: ${count}")
         OutlinedTextField(
             value = name,
-            onValueChange = { viewModel.updateName(it?:"") }
+            onValueChange = { viewModel.updateName(it?:"") },
+            label = { Text(text = "Name")}
         )
-        OutlinedTextField(value = age, onValueChange = {
+        Text(text = name)
+        OutlinedTextField(value = age,
+            onValueChange = {
             val newAge = it.toIntOrNull()?:0
             viewModel.updateAge(newAge)
-        })
+        },
+            label = { Text(text = "Age")})
+        Text(text = age)
         Row {
             Button(onClick = { viewModel.increment(1) }) {
                 Text(text = "Increment")
--- app/src/main/java/com/nhinhnguyenuit/mvvmbasic/viewmodel/CounterViewModel.kt
+++ app/src/main/java/com/nhinhnguyenuit/mvvmbasic/viewmodel/CounterViewModel.kt
@@ -1,44 +1,42 @@
 package com.nhinhnguyenuit.mvvmbasic.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nhinhnguyenuit.mvvmbasic.model.CounterState
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.math.BigDecimal
 
 class CounterViewModel : ViewModel() {
-    private val _state = MutableStateFlow(CounterState())
-    val state: StateFlow<CounterState> get() = _state
+    private val _state = MutableLiveData(CounterState())
+    val state: LiveData<CounterState> get() = _state
 
     fun increment(num: Int) {
         viewModelScope.launch {
-            val newCount = state.value
+            val newCount = state.value?: CounterState()
             _state.value = newCount.copy(count = newCount.count + num)
         }
     }
 
     fun decrement(num: Int) {
         viewModelScope.launch {
-            val newCount = state.value
+            val newCount = state.value?: CounterState()
             _state.value = newCount.copy(count = newCount.count - num)
         }
     }
 
     fun updateName(newName: String) {
         viewModelScope.launch {
-            val currentName = _state.value
+            val currentName = _state.value?:CounterState()
             _state.value = currentName.copy(name = newName)
         }
     }
 
     fun updateAge(newAge: Int) {
         viewModelScope.launch {
-            val currentAge = _state.value
+            val currentAge = _state.value?:CounterState()
             _state.value = currentAge.copy(age = newAge)
         }
     }
